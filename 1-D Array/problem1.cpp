50407a392ac7405494784a19cba6b8d47ef53187
Chitra Nayal <43963817+nayalchitra@users.noreply.github.com>
2019-06-09 20:54:30 +0530

    Create problem1.cpp

---
+++ 1-D Array/problem1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,n;
+	cin>>n;
+	int a[n];
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	int b;
+	cin>>b;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==b)
+		{
+			cout<<i;
+			return 0;
+		}
+	}
+	cout<<"number does not exists";
+}
