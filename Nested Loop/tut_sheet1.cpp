a4436e29c222c3f0ca53a501f1d555bbcb41a4b5
Chitra Nayal <43963817+nayalchitra@users.noreply.github.com>
2019-06-09 20:39:47 +0530

    Create tut_sheet1.cpp

---
+++ Nested Loop/tut_sheet1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,i,j,k;
+	cin>>n;
+	int a[n];
+	cin>>k;
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]+a[j]==k)
+			{
+				cout<<"yes";
+				return 0;
+			}
+		}
+	}
+	cout<<"no";
+}
