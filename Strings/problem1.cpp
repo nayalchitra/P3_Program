4746daff65023d71a5615e9ac4937cd4bb785cae
Chitra Nayal <43963817+nayalchitra@users.noreply.github.com>
2019-06-29 21:33:44 +0530

    Strings

--- Strings/problem1.cpp
+++ Strings/problem1.cpp
@@ -1,27 +1,37 @@
+/* sum of two large numbers 
+	Given two numbers as strings. The numbers may be very large, the task is to find sum of these two numbers
+*/
 
-// convert the input string fro lowercase  to upper case
-
-#include<iostream>
-#include<string.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-void convertLowerToUpper(char c[])
-{
-	for(int i=0;i<strlen(c);i++)
-	{
-		if(c[i]>='a' && c[i]<='z' )
-			c[i]=c[i]+'A'-'a';
-	}
-	cout<<c;
-}
-
 
 int main()
 {
-	
-	char c[20];
-	cin>>c;
-	convertLowerToUpper(c);
-}
-	
-  
+	string s1,s2;
+	cin>>s1>>s2;
+	string sum="";
+	int l1=s1.length()-1;
+	int l2=s2.length()-1;
+	int carry=0;
+	int num=0;
+	while(l1>=0||l2>=0)
+	{
+		if(s1[l1]!='\0')
+			num=s1[l1]-'0';
+		else
+			num=0;
+		if(s2[l2]!='\0')
+			num+=s2[l2]-'0';
+		else
+			num+=0;
+		num+=carry;
+		sum+=(num%10)+'0';
+		carry=num/10;
+		l1--;
+		l2--;
+	}
+	if(carry)
+		sum+=carry+'0';
+	reverse(sum.begin(),sum.end());
+	cout<<sum<<endl;
+} 
