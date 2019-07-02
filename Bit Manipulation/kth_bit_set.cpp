54ae65e5baf8e916dd32f65a1419e578576649e7
Chitra Nayal <43963817+nayalchitra@users.noreply.github.com>
2019-07-02 21:49:52 +0530

    Bit Manipulation ..!!

---
+++ Bit Manipulation/kth_bit_set.cpp
@@ -0,0 +1,15 @@
+
+// check if the kth bit of a  number is set
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	if(n & 1<<k)
+		cout<<"Yes";
+	else
+		cout<<"No";
+}
