463718821bc7bd21abe8413fc87726abd3dfead4
Chitra Nayal <43963817+nayalchitra@users.noreply.github.com>
2022-09-18 21:34:44 +0530

    Update power_of_2.cpp

--- Bit Manipulation/power_of_2.cpp
+++ Bit Manipulation/power_of_2.cpp
@@ -1,5 +1,6 @@
 // program to check if a number is a power of 2?
 
+//method 1: using bit manipulation
 #include<iostream>
 using namespace std;
 
@@ -18,4 +19,21 @@ int main()
 		cout<<"Yes";
 	else
 		cout<<"No";
+}
+
+
+
+// method 2 : using log2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>> n;
+	if(ceil(log2(n)) == floor(log2(n)) )
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
 }
